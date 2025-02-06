57f721a4f4486a6990fc3140fa2dc6f38c0928a6
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-02-06 17:48:01 +0000

    React

--- Contxtapi/src/App.jsx
+++ Contxtapi/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <UserContextProvider>
       <div className='bg-zinc-800 min-h-screen w-full '>
+       < Login />
        
       </div>
     </UserContextProvider>
