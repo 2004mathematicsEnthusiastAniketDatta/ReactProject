78a721a68583ec05be0c1be4325ebb55f6b9737b
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-02-08 05:48:24 +0000

    Error

--- Contxtapi/src/App.jsx
+++ Contxtapi/src/App.jsx
@@ -1,20 +1,42 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
 import './App.css'
-import UserContextProvider from './context/UserContextProvider'
-import Login from './components/Login'
-import Profile from './components/Profile'
+import { ThemeProvider } from './context/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
 
   return (
-    <UserContextProvider>
-      <div className='bg-zinc-800 min-h-screen w-full '>
-       < Login />
-       <Profile />
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
       </div>
-    </UserContextProvider>
+    </ThemeProvider>
   )
 }
 
